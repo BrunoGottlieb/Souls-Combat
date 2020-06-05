68a3afd9c764cd5bbfbec16e32f5da0867fab504
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2020-06-05 04:11:31 -0300

    04-06-20 - Stones on ground

--- Assets/Scripts/IKFootPlacement.cs
+++ Assets/Scripts/IKFootPlacement.cs
@@ -50,18 +50,23 @@ public class IKFootPlacement : MonoBehaviour
 
         RaycastHit hit;
         Ray ray = new Ray(anim.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
-    
-        if(Physics.Raycast(ray, out hit, distanceToGround + 1f, layerMask))
+
+        if (Physics.Raycast(ray, out hit, distanceToGround + 1f, layerMask))
         {
-            if(hit.transform.tag == "Walkable")
+            if(this.gameObject.tag == "Boss")
+                print(hit.transform.name);
+            if (hit.transform.tag == "Walkable")
             {
+                if (hit.transform.name.Contains("Sand")) print("Sand");
+                else print("Stone");
+
                 Vector3 footPosition = hit.point;
                 footPosition.y += distanceToGround;
                 anim.SetIKPosition(AvatarIKGoal.LeftFoot, footPosition);
                 Vector3 forward = Vector3.ProjectOnPlane(transform.forward, hit.normal);
                 anim.SetIKRotation(AvatarIKGoal.LeftFoot, Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation);
             }
-            
+
         }
 
         ray = new Ray(anim.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
