86d63e2b85eb033a5a3c4fbe838507de3558987d
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2020-06-09 16:14:36 -0300

    09-06-20 - Sparks on hit Rocks

--- Assets/Scripts/SwordScript.cs
+++ Assets/Scripts/SwordScript.cs
@@ -7,6 +7,7 @@ public class SwordScript : MonoBehaviour
     public Animator girlAnim;
     public AudioClip[] sparkSound; // som quando a espada bate em algo
     private AudioSource sparksSource; // audioSource que ira tocar o som, aleatoriamente
+    public GameObject sparkEffect;
 
     private void Start()
     {
@@ -15,11 +16,23 @@ public class SwordScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.layer == 13 && girlAnim.GetBool("Attacking") && !sparksSource.isPlaying) // caso seja um elemento do cenario
+        if (other.gameObject.layer == 13 && girlAnim.GetBool("Attacking") && !sparksSource.isPlaying) // caso seja um elemento do cenario
         {
             sparksSource.clip = sparkSound[Random.Range(0, sparkSound.Length)];
             sparksSource.Play();
+            if (other.transform.parent.gameObject.name.Contains("Rochas")) // caso seja uma rocha, instancia o efeito
+            {
+                GameObject spark = Instantiate(sparkEffect, this.transform.position, Quaternion.identity);
+                Destroy(spark, 0.3f);
+            }
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Colidiu");
+        GameObject spark = Instantiate(sparkEffect, collision.contacts[0].point, Quaternion.identity);
+        Destroy(spark, 1);
     }
 
 }
